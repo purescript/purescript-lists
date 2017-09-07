5bfc1769d0212c4cb1fe1850e6462486b9bd4df4
Giorgi Bagdavadze <notgiorgi@gmail.com>
2017-09-07 19:27:36 +0400

    fix test labels

--- test/Test/Data/List.purs
+++ test/Test/Data/List.purs
@@ -226,7 +226,7 @@ testList = do
   assert $ (take 2 (l [1, 2, 3])) == l [1, 2]
   assert $ (take 1 nil) == nil
 
-  log "take should keep the specified number of items from the end of an list, discarding the rest"
+  log "takeEnd should keep the specified number of items from the end of an list, discarding the rest"
   assert $ (takeEnd 1 (l [1, 2, 3])) == l [3]
   assert $ (takeEnd 2 (l [1, 2, 3])) == l [2, 3]
   assert $ (takeEnd 1 nil) == nil
@@ -236,7 +236,7 @@ testList = do
   assert $ (takeWhile (_ /= 3) (l [1, 2, 3])) == l [1, 2]
   assert $ (takeWhile (_ /= 1) nil) == nil
 
-  log "drop should remove the specified number of items from the front of an list"
+  log "dropEnd should remove the specified number of items from the end of an list"
   assert $ (drop 1 (l [1, 2, 3])) == l [2, 3]
   assert $ (drop 2 (l [1, 2, 3])) == l [3]
   assert $ (drop 1 nil) == nil
