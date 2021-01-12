178f1a29c9b8499d0a3e4ec6e2895eaa4bfe007d
Nicholas Scheel <monoidmusician@gmail.com>
2021-01-12 14:03:36 -0600

    Specialize to Set

--- src/Data/List/Internal.purs
+++ src/Data/List/Internal.purs
@@ -4,60 +4,60 @@ import Prelude
 
 import Data.List.Types (List(..))
 
-data Map k v
+data Set k
   = Leaf
-  | Two (Map k v) k v (Map k v)
-  | Three (Map k v) k v (Map k v) k v (Map k v)
+  | Two (Set k) k (Set k)
+  | Three (Set k) k (Set k) k (Set k)
 
-data TreeContext k v
-  = TwoLeft k v (Map k v)
-  | TwoRight (Map k v) k v
-  | ThreeLeft k v (Map k v) k v (Map k v)
-  | ThreeMiddle (Map k v) k v k v (Map k v)
-  | ThreeRight (Map k v) k v (Map k v) k v
+data TreeContext k
+  = TwoLeft k (Set k)
+  | TwoRight (Set k) k
+  | ThreeLeft k (Set k) k (Set k)
+  | ThreeMiddle (Set k) k k (Set k)
+  | ThreeRight (Set k) k (Set k) k
 
-fromZipper :: forall k v. Ord k => List (TreeContext k v) -> Map k v -> Map k v
+fromZipper :: forall k. Ord k => List (TreeContext k) -> Set k -> Set k
 fromZipper Nil tree = tree
 fromZipper (Cons x ctx) tree =
   case x of
-    TwoLeft k1 v1 right -> fromZipper ctx (Two tree k1 v1 right)
-    TwoRight left k1 v1 -> fromZipper ctx (Two left k1 v1 tree)
-    ThreeLeft k1 v1 mid k2 v2 right -> fromZipper ctx (Three tree k1 v1 mid k2 v2 right)
-    ThreeMiddle left k1 v1 k2 v2 right -> fromZipper ctx (Three left k1 v1 tree k2 v2 right)
-    ThreeRight left k1 v1 mid k2 v2 -> fromZipper ctx (Three left k1 v1 mid k2 v2 tree)
+    TwoLeft k1 right -> fromZipper ctx (Two tree k1 right)
+    TwoRight left k1 -> fromZipper ctx (Two left k1 tree)
+    ThreeLeft k1 mid k2 right -> fromZipper ctx (Three tree k1 mid k2 right)
+    ThreeMiddle left k1 k2 right -> fromZipper ctx (Three left k1 tree k2 right)
+    ThreeRight left k1 mid k2 -> fromZipper ctx (Three left k1 mid k2 tree)
 
-data KickUp k v = KickUp (Map k v) k v (Map k v)
+data KickUp k = KickUp (Set k) k (Set k)
 
 -- | Insert or replace a key/value pair in a map
-insert :: forall k v. Ord k => k -> v -> Map k v -> Map k v
-insert k v = down Nil
+insert :: forall k. Ord k => k -> Set k -> Set k
+insert k = down Nil
   where
   comp :: k -> k -> Ordering
   comp = compare
 
-  down :: List (TreeContext k v) -> Map k v -> Map k v
-  down ctx Leaf = up ctx (KickUp Leaf k v Leaf)
-  down ctx (Two left k1 v1 right) =
+  down :: List (TreeContext k) -> Set k -> Set k
+  down ctx Leaf = up ctx (KickUp Leaf k Leaf)
+  down ctx (Two left k1 right) =
     case comp k k1 of
-      EQ -> fromZipper ctx (Two left k v right)
-      LT -> down (Cons (TwoLeft k1 v1 right) ctx) left
-      _  -> down (Cons (TwoRight left k1 v1) ctx) right
-  down ctx (Three left k1 v1 mid k2 v2 right) =
+      EQ -> fromZipper ctx (Two left k right)
+      LT -> down (Cons (TwoLeft k1 right) ctx) left
+      _  -> down (Cons (TwoRight left k1) ctx) right
+  down ctx (Three left k1 mid k2 right) =
     case comp k k1 of
-      EQ -> fromZipper ctx (Three left k v mid k2 v2 right)
+      EQ -> fromZipper ctx (Three left k mid k2 right)
       c1 ->
         case c1, comp k k2 of
-          _ , EQ -> fromZipper ctx (Three left k1 v1 mid k v right)
-          LT, _  -> down (Cons (ThreeLeft k1 v1 mid k2 v2 right) ctx) left
-          GT, LT -> down (Cons (ThreeMiddle left k1 v1 k2 v2 right) ctx) mid
-          _ , _  -> down (Cons (ThreeRight left k1 v1 mid k2 v2) ctx) right
+          _ , EQ -> fromZipper ctx (Three left k1 mid k right)
+          LT, _  -> down (Cons (ThreeLeft k1 mid k2 right) ctx) left
+          GT, LT -> down (Cons (ThreeMiddle left k1 k2 right) ctx) mid
+          _ , _  -> down (Cons (ThreeRight left k1 mid k2) ctx) right
 
-  up :: List (TreeContext k v) -> KickUp k v -> Map k v
-  up Nil (KickUp left k' v' right) = Two left k' v' right
+  up :: List (TreeContext k) -> KickUp k -> Set k
+  up Nil (KickUp left k' right) = Two left k' right
   up (Cons x ctx) kup =
     case x, kup of
-      TwoLeft k1 v1 right, KickUp left k' v' mid -> fromZipper ctx (Three left k' v' mid k1 v1 right)
-      TwoRight left k1 v1, KickUp mid k' v' right -> fromZipper ctx (Three left k1 v1 mid k' v' right)
-      ThreeLeft k1 v1 c k2 v2 d, KickUp a k' v' b -> up ctx (KickUp (Two a k' v' b) k1 v1 (Two c k2 v2 d))
-      ThreeMiddle a k1 v1 k2 v2 d, KickUp b k' v' c -> up ctx (KickUp (Two a k1 v1 b) k' v' (Two c k2 v2 d))
-      ThreeRight a k1 v1 b k2 v2, KickUp c k' v' d -> up ctx (KickUp (Two a k1 v1 b) k2 v2 (Two c k' v' d))
+      TwoLeft k1 right, KickUp left k' mid -> fromZipper ctx (Three left k' mid k1 right)
+      TwoRight left k1, KickUp mid k' right -> fromZipper ctx (Three left k1 mid k' right)
+      ThreeLeft k1 c k2 d, KickUp a k' b -> up ctx (KickUp (Two a k' b) k1 (Two c k2 d))
+      ThreeMiddle a k1 k2 d, KickUp b k' c -> up ctx (KickUp (Two a k1 b) k' (Two c k2 d))
+      ThreeRight a k1 b k2, KickUp c k' d -> up ctx (KickUp (Two a k1 b) k2 (Two c k' d))
